32283aa96c88cb2307cc2f5d4bc1780b53780dd2
physikerwelt <wiki@physikerwelt.de>
2015-10-02 19:58:27 +0200

    Output all tokens
    * Now all tokens are exported not only the leaf-like tokens

--- test/tokenizer.js
+++ test/tokenizer.js
@@ -2,29 +2,32 @@
 var assert = require('assert');
 var texvc = require("texvcjs");
 var lister = require('../lib/tokenLister');
-
 var testcases = [
     {in: '', out: []},
-    {in: 'a', out: [['TEX_ONLY', 'a']]},
-    {in: 'a^2', out: [['TEX_ONLY', 'a'], ['TEX_ONLY', '2']]},
+    {in: 'a', out: [['LITERAL', ''], ['TEX_ONLY', 'a']]},
+    {in: 'a^2', out: [['UQ', ''], ['LITERAL', ''], ['TEX_ONLY', 'a'], ['LITERAL', ''], ['TEX_ONLY', '2']]},
     {
-        in: 'a^2+b^{2}',
-        out: [['TEX_ONLY', 'a'], ['TEX_ONLY', '2'], ['TEX_ONLY', '+'], ['TEX_ONLY', 'b'], ['TEX_ONLY', '2']]
+        in: 'a^2+b^2',
+        out: [['UQ', ''], ['LITERAL', ''], ['TEX_ONLY', 'a'], ['LITERAL', ''], ['TEX_ONLY', '2'],
+            ["LITERAL", ""], ["TEX_ONLY", "+"],
+            ['UQ', ''], ['LITERAL', ''], ['TEX_ONLY', 'b'], ['LITERAL', ''], ['TEX_ONLY', '2']]
     },
     {
         in: 'a^{2}+b^{2}',
-        out: [['TEX_ONLY', 'a'], ['TEX_ONLY', '2'], ['TEX_ONLY', '+'], ['TEX_ONLY', 'b'], ['TEX_ONLY', '2']]
+        out: [['UQ', ''], ['LITERAL', ''], ['TEX_ONLY', 'a'], ['CURLY', ''], ['LITERAL', ''], ['TEX_ONLY', '2'],
+            ["LITERAL", ""], ["TEX_ONLY", "+"],
+            ['UQ', ''], ['LITERAL', ''], ['TEX_ONLY', 'b'], ['CURLY', ''], ['LITERAL', ''], ['TEX_ONLY', '2']]
     },
     {
         in: '\\frac2b',
-        out: [['FUN2', '\\frac'], ['TEX_ONLY', '2'], ['TEX_ONLY', 'b']]
+        out: [['FUN2', '\\frac'], ['LITERAL', ''], ['TEX_ONLY', '2'], ['LITERAL', ''], ['TEX_ONLY', 'b']]
     }
 ];
 
 describe('Render', function () {
     testcases.forEach(function (tc) {
         var input = tc.in;
-        var output = tc.out || input;
+        var output = tc.out;
         it('should correctly render ' + JSON.stringify(input), function () {
             assert.deepEqual(lister(texvc.parse(input)), output);
         });
