743a5e497aa92b489231da06bbc16dd3a63be9f1
physikerwelt <wiki@physikerwelt.de>
2015-10-05 16:46:42 +0200

    Ignore differential d operator in integrals
    * e.g. \int f(x) dx should identify f and x but not d.

--- test/identifier.js
+++ test/identifier.js
@@ -12,9 +12,7 @@ var testcases = [
     {in: 't_a', out: ['t_{a}']},
     {
         in: "\\hat{U}(t,t_0)=\\exp{\\left(-\\frac{i}\\hbar \\int_{t_0}^t \\hat{H}(t')dt'\\right)}",
-        out: ['\\hat{U}', 't', 't_{0}', 'i', '\\hbar', 't_{0}', 't', '\\hat{H}', 't\'', 'd', //not rally but...
-            't\''
-        ]
+        out: ['\\hat{U}', 't', 't_{0}', 'i', '\\hbar', 't_{0}', 't', '\\hat{H}', 't\'', 't\'']
     },
     {in: "\\begin{align}\n  &[\\mathrm j_k, \\mathrm j_l]\n" +
         "    \\equiv \\mathrm j_k \\mathrm j_l - \\mathrm j_l \\mathrm j_k\n" +
@@ -23,14 +21,16 @@ var testcases = [
         "\\end{align}",
     out:["\\mathrm{j}_{k}","\\mathrm{j}_{l}","\\mathrm{j}_{k}","\\mathrm{j}_{l}","\\mathrm{j}_{l}","\\mathrm{j}_{k}",
         "i","\\hbar","m","\\varepsilon_{k,l,m}","\\mathrm{j}_{m}","k","l","m","\\mathrm{x}","\\mathrm{y}","\\mathrm{z}"]
-    }
+    },
+    {in:"x = \\int_1^y {\\mathrm{d}t \\over t}",
+    out:["x","y","t","t"]}
 ];
 
-describe('Identifier', function () {
+describe('Identifiers', function () {
     testcases.forEach(function (tc) {
         var input = tc.in;
         var output = tc.out;
-        it('should correctly render ' + JSON.stringify(input), function () {
+        it('should be discovered ' + JSON.stringify(input), function () {
             assert.deepEqual(lister(texvc.parse(input)), output);
         });
     });
