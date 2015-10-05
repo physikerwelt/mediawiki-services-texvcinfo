da05606d52d33296f9ab9203e6c035a33b781bca
physikerwelt <wiki@physikerwelt.de>
2015-10-05 14:33:09 +0200

    Add additional tests.

--- test/identifier.js
+++ test/identifier.js
@@ -9,12 +9,20 @@ var testcases = [
     {in: 'a^2+b^2', out: ['a', 'b']},
     {in: 'a^{2}+b^{2}', out: ['a', 'b']},
     {in: '\\frac2b', out: ['b']},
-    {in: 't_a', out: ['t_a']},
+    {in: 't_a', out: ['t_{a}']},
     {
         in: "\\hat{U}(t,t_0)=\\exp{\\left(-\\frac{i}\\hbar \\int_{t_0}^t \\hat{H}(t')dt'\\right)}",
-        out: ['\\hat{U}', 't', 't_0', 'i', '\\hbar', 't_0', 't', '\\hat{H}', 't\'', 'd', //not rally but...
+        out: ['\\hat{U}', 't', 't_{0}', 'i', '\\hbar', 't_{0}', 't', '\\hat{H}', 't\'', 'd', //not rally but...
             't\''
         ]
+    },
+    {in: "\\begin{align}\n  &[\\mathrm j_k, \\mathrm j_l]\n" +
+        "    \\equiv \\mathrm j_k \\mathrm j_l - \\mathrm j_l \\mathrm j_k\n" +
+        "    = i \\hbar \\sum_m \\varepsilon_{k, l, m} \\mathrm j_m\n" +
+        "    & k, l, m &\\in \\{\\mathrm x, \\mathrm y, \\mathrm z\\}\n" +
+        "\\end{align}",
+    out:["\\mathrm{j}_{k}","\\mathrm{j}_{l}","\\mathrm{j}_{k}","\\mathrm{j}_{l}","\\mathrm{j}_{l}","\\mathrm{j}_{k}",
+        "i","\\hbar","m","\\varepsilon_{k,l,m}","\\mathrm{j}_{m}","k","l","m","\\mathrm{x}","\\mathrm{y}","\\mathrm{z}"]
     }
 ];
 
