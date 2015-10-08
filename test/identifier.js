b89eaa9344b8fd39cd79c76a7ab0ec6b6b7a57ed
physikerwelt <wiki@physikerwelt.de>
2015-10-08 19:31:01 +0200

    Fix optional arguments
    * \sqrt[a]{b} is now outputs [a,b] without \sqrt

--- test/identifier.js
+++ test/identifier.js
@@ -11,6 +11,8 @@ var testcases = [
     {in: '\\frac2b', out: ['b']},
     {in: 't_a', out: ['t_{a}']},
     {in: '\\mathrm{kg}', out: ['\\mathrm{kg}']},
+    {in: '\\sqrt[3]{81}', out:[]},
+    //{in: 'a_{bc}', out: ['a_{bc}']},
     {
         in: "\\hat{U}(t,t_0)=\\exp{\\left(-\\frac{i}\\hbar \\int_{t_0}^t \\hat{H}(t')dt'\\right)}",
         out: ['\\hat{U}', 't', 't_{0}', 'i', '\\hbar', 't_{0}', 't', '\\hat{H}', 't\'', 't\'']
@@ -47,8 +49,11 @@ var testcases = [
     {
         in: "\\int_{R_n} \\cdots \\int_{R_2} \\int_{R_1} f(x_1, x_2, \\ldots, x_n)" +
         " \\, dx_1 dx_2\\cdots dx_n \\equiv \\int_R f(\\boldsymbol{x}) \\, d^n\\boldsymbol{x}",
-        out: ['R_{n}', 'R_{2}', 'R_{1}', 'f', 'x_{1}', 'x_{2}', 'x_{n}', 'x_{1}', 'x_{2}', 'x_{n}', 'R', 'f', '\\boldsymbol{x}', 'n', '\\boldsymbol{x}']
-    }
+        out: ['R_{n}', 'R_{2}', 'R_{1}', 'f', 'x_{1}', 'x_{2}', 'x_{n}',
+            'x_{1}', 'x_{2}', 'x_{n}', 'R', 'f', '\\boldsymbol{x}', 'n', '\\boldsymbol{x}']
+    },
+    //{in: "\\reals", out:["\\reals"]},
+    //{in: "\\mathrm {MTF}_{display}(\\xi,\\eta)", out: ["\\mathrm{MTF}_{display}", "\\xi", "\\eta"]}
 ];
 
 describe('Identifiers', function () {
