302dd4887939c3de392f8d07d90e67fd22070016
Moritz Schubotz <wiki@physikerwelt.de>
2016-05-14 22:10:47 -0400

    Extend test coverage for lib/index
    Now 100%

--- test/index.js
+++ test/index.js
@@ -45,6 +45,31 @@ var testcases = [
             tree: [["FUN1nb", ["\\mathbb"], ["CURLY", [["LITERAL", ["TEX_ONLY", ["x"]]]]]]],
             list: [["FUN1nb", "\\mathbb"], ["CURLY", ""], ["LITERAL", ""], ["TEX_ONLY", "x"]]
         }
+    }, {
+        input: '\\invalid',
+        out: {
+            "column": undefined,
+            "details": "\\invalid",
+            "line": undefined,
+            "offset": undefined,
+            "status": "F"
+        }
+    }, {
+        input: '\\left(',
+        out: {
+            "column": undefined,
+            "details": "SyntaxError: Expected \"-\", \"[\", \"\\\\\", \"\\\\begin\", \"\\\\begin{\", \"]\", \"^\", \"_\", \"{\", [ \\t\\n\\r], [%$], [().], [,:;?!\\'], [-+*=], [0-9], [><~], [\\/|] or [a-zA-Z] but end of input found.",
+            "line": undefined,
+            "offset": undefined,
+            "status": "S"
+        }
+    }, {
+        input: {length: 1}, // Artificial object to create an error
+        options: {format: "json", flatTree: true},
+        out: {
+            "details": "TypeError: Cannot read property 'length' of undefined",
+            "status": "-"
+        }
     }
 ];
 
@@ -57,4 +82,15 @@ describe('Index', function () {
             assert.deepEqual(texvcinfo.texvcinfo(input, options), output);
         });
     });
+    it('should throw an exception in debug mode', function () {
+        try {
+            // assert.throws might be alternative
+            texvcinfo.texvcinfo('\\left(', {debug: true});
+            assert.fail('this code should not be reached');
+        } catch (e) {
+            assert.equal(e.name, 'SyntaxError');
+        }
+
+    });
 });
+
