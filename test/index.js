5e6201686abe9d8d5065e736168a7b1191087a6c
Moritz Schubotz <wiki@physikerwelt.de>
2016-05-14 21:05:33 -0400

    Extend test coverage for lib/index

--- test/index.js
+++ test/index.js
@@ -8,6 +8,44 @@ var testcases = [
         options: {format: "tree"},
         out: [["FUN1nb", ["\\mathbb"], ["CURLY", [["LITERAL", ["TEX_ONLY", ["x"]]]]]]]
     },
+    {
+        input: '\\mathbb{x}',
+        options: {format: "identifier"},
+        out: ['\\mathbb{x}']
+    },
+    {
+        input: '\\mathbb{x}',
+        options: {format: "list"},
+        out: [["FUN1nb", "\\mathbb"], ["CURLY", ""], ["LITERAL", ""], ["TEX_ONLY", "x"]]
+    }, {
+        input: '\\mathbb{x}',
+        options: {format: "json"},
+        out: {
+            "name": "root",
+            "children": [{
+                "name": "FUN1nb",
+                "children": [{"name": "\\mathbb"}, {
+                    "name": "CURLY",
+                    "children": [{
+                        "name": "root",
+                        "children": [{
+                            "name": "LITERAL",
+                            "children": [{"name": "TEX_ONLY", "children": [{"name": "x"}]}]
+                        }]
+                    }]
+                }]
+            }]
+        }
+
+    }, {
+        input: '\\mathbb{x}',
+        options: {format: "all"},
+        out: {
+            identifier: ['\\mathbb{x}'],
+            tree: [["FUN1nb", ["\\mathbb"], ["CURLY", [["LITERAL", ["TEX_ONLY", ["x"]]]]]]],
+            list: [["FUN1nb", "\\mathbb"], ["CURLY", ""], ["LITERAL", ""], ["TEX_ONLY", "x"]]
+        }
+    }
 ];
 
 describe('Index', function () {
@@ -15,7 +53,7 @@ describe('Index', function () {
         var input = tc.input;
         var options = tc.options;
         var output = tc.out;
-        it('should correctly render ' + JSON.stringify(input), function () {
+        it('should correctly handle ' + JSON.stringify(input) + 'with option' + JSON.stringify(options), function () {
             assert.deepEqual(texvcinfo.texvcinfo(input, options), output);
         });
     });
